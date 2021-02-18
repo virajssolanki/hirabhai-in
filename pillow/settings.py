6718faefe0ddd7b153fd3ba3c1925751e6ab81cc
Viraj Solanki <virazssolanki@gmail.com>
2021-02-18 17:09:34 +0530

    static

--- pillow/settings.py
+++ pillow/settings.py
@@ -138,7 +138,6 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
