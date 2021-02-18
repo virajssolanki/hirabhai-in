4e3fd0bca9de5d869c4516263b7ec627f93c3e35
Viraj Solanki <virazssolanki@gmail.com>
2021-02-18 17:50:32 +0530

    static

--- pillow/settings.py
+++ pillow/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'gc-9ut1^^#8@+2lb$^6ph)=fvw+!drexz94tzm=95e6fm@o2i3'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
