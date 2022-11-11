f68dc98f8836b2635d0908b9ca2b8e53b3f004b0
viraj <virazssolanki@gmail.com>
2022-11-12 03:48:28 +0530

    new changes

--- main/forms.py
+++ main/forms.py
@@ -20,12 +20,19 @@ SITES = (
     ('કીંદરવા ', 'કીંદરવા'),
     )
 
+default_error_messages = {
+    'max_length': 'Enter a valid 10 digit mobile number',
+    'min_length': 'Enter a valid 10 digit mobile number',
+    'required': 'This field is required',
+    'invalid': 'Enter a valid value'
+}
+
 class UimgForm(forms.Form):
     name = forms.CharField(required=False)
     # village = forms.ChoiceField(choices = SITES) 
     village = forms.CharField(required=False) 
     # number = forms.CharField(required=False)
-    number = forms.IntegerField(min_value=1000000000, max_value=9999999999)
+    number = forms.IntegerField(min_value=1000000000, max_value=9999999999, error_messages=my_default_errors)
     img = forms.ImageField()
     x = forms.FloatField(widget=forms.HiddenInput())
     y = forms.FloatField(widget=forms.HiddenInput())
